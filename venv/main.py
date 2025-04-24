7cab448116b81da8ee6c3abc85b9a11a4f7312b0
fagpaf <fagpaf@cin.ufpe.br>
2025-04-24 17:55:18 -0300

    att o jupyter

--- venv/main.py
+++ venv/main.py
@@ -1,2 +1,10 @@
-x = int(input())
-print(x)
+# import numpy as np
+# data = [np.random.standard_normal() for i in range(7)]
+# print(data)
+
+a = 4
+b = 1
+if (a and b) > 0:
+    print(a+b)
+else:
+    print("0")
